09a6dedfe2da30e9de87fb7b1c3746bfa3e7d050
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-10-13 14:33:47 -0700

    Changed custom active classname to fit css module

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -7,18 +7,37 @@ const NavLinks = (props) => {
   return (
     <ul className={styles.navLinks}>
       <li>
-        <NavLink to="/" exact>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? styles.active : '')}
+          exact
+        >
           All Users
         </NavLink>
       </li>
       <li>
-        <NavLink to="/u1/places">My Places</NavLink>
+        <NavLink
+          to="/u1/places"
+          className={({ isActive }) => (isActive ? styles.active : '')}
+        >
+          My Places
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/places/new">Add Places</NavLink>
+        <NavLink
+          to="/places/new"
+          className={({ isActive }) => (isActive ? styles.active : '')}
+        >
+          Add Places
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/auth">Authenticate</NavLink>
+        <NavLink
+          to="/auth"
+          className={({ isActive }) => (isActive ? styles.active : '')}
+        >
+          Authenticate
+        </NavLink>
       </li>
     </ul>
   );
