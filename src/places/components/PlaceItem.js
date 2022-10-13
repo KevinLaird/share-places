839df99ef140af3f2786411069d110f2808f70a6
Kevin Laird <107648990+KevinLairdHatchways@users.noreply.github.com>
2022-10-13 15:39:58 -0700

    Added Modal component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button/Button';
+import Modal from '../../shared/components/UIElements/Modal';
 
 import styles from './PlaceItem.module.css';
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const toggleMapHandler = () => setShowMap(!showMap);
+
   return (
-    <li className={styles.placeItem}>
-      <Card className={styles.content}>
-        <div className={styles.image}>
-          <img src={props.image} alt={props.title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={toggleMapHandler}
+        header={props.address}
+        contentClass={styles.modalContent}
+        footerClass={styles.modalActions}
+        footer={<Button onClick={toggleMapHandler}>CLOSE</Button>}
+      >
+        <div className={styles.mapContainer}>
+          <h2>The Map!</h2>
         </div>
-        <div className={styles.info}>
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className={styles.actions}>
-          <Button inverse>VIEW ON MAP</Button>
-          <Button to={`/places/${props.id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className={styles.placeItem}>
+        <Card className={styles.content}>
+          <div className={styles.image}>
+            <img src={props.image} alt={props.title} />
+          </div>
+          <div className={styles.info}>
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className={styles.actions}>
+            <Button inverse onClick={toggleMapHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
