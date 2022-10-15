83c277c5bf897f958643126de461dc6c4cfe28dc
KevinLaird <kevinlaird16@gmail.com>
2022-10-15 16:31:29 -0700

    Added authentication component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Users from './user/pages/Users';
+import Authenticate from './user/pages/Authenticate';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
@@ -17,6 +18,7 @@ function App() {
           <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="/places/new" element={<NewPlace />} />
           <Route path="/places/:placeId" element={<UpdatePlace />} />
+          <Route path="/auth" element={<Authenticate />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
