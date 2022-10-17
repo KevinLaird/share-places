a7c2b5f1a6870cc5e5ddce756bcddb6b02d887cf
KevinLaird <kevinlaird16@gmail.com>
2022-10-17 15:47:36 -0700

    Added dummy data & 2 get routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in Places');
-  res.json({ message: 'It works!' });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire state building',
+    description: 'Content',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/250px-Empire_State_Building_%28aerial_view%29.jpg',
+    address: `350 Fifth Avenue[a] Manhattan, New York`,
+    location: {
+      lat: 40.7484213,
+      lng: -73.9878416,
+    },
+    creator: 'u1',
+  },
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const placeJson = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  res.json({ placeJson });
+});
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+
+  const user = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  res.json({ user });
 });
 
 module.exports = router;
