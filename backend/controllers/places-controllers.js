83e7b272e25b2659dcb05eed25dd145f9888da09
KevinLaird <kevinlaird16@gmail.com>
2022-10-19 16:04:32 -0700

    Now gets places by userid through user relation

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -35,25 +35,27 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate('places');
   } catch (err) {
     const error = new HttpError(
-      'Fetchning places failed, please try again later.',
+      'Fetching places failed, please try again later.',
       500,
     );
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError('Could not find any places by the user id.', 404),
     );
   }
 
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true }),
+    ),
   });
 };
 
