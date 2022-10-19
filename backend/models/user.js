48d1f5db49627e37e37e78dedf2c7be74c059859
KevinLaird <kevinlaird16@gmail.com>
2022-10-19 13:58:04 -0700

    User Schema now checked for unique validation.

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,3 +10,7 @@ const userSchema = new Schema({
   image: { type: String, required: true },
   places: { type: String, required: true },
 });
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User');
