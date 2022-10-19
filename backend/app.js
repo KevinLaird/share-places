3429722cde590dbf4bff4493c5c8be7ddc080f32
KevinLaird <kevinlaird16@gmail.com>
2022-10-18 20:55:04 -0700

    Connected MongoDB to backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config();
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -25,4 +30,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    `mongodb+srv://Kevin:${MONGODB_PASSWORD}@cluster0.xsfwabo.mongodb.net/places?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
