352e237886c159b9f653b6b245df9b9c3b6f2d40
KevinLaird <kevinlaird16@gmail.com>
2022-10-18 21:53:40 -0700

    Removed DUMMY_PLACES temp data

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -5,35 +5,6 @@ const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
 const Place = require('../models/place');
 
-let DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire state building',
-    description: 'Content',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/250px-Empire_State_Building_%28aerial_view%29.jpg',
-    address: `350 Fifth Avenue[a] Manhattan, New York`,
-    location: {
-      lat: 40.7484213,
-      lng: -73.9878416,
-    },
-    creator: 'u1',
-  },
-  {
-    id: 'p2',
-    title: 'Empire state building',
-    description: 'Content',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/250px-Empire_State_Building_%28aerial_view%29.jpg',
-    address: `350 Fifth Avenue[a] Manhattan, New York`,
-    location: {
-      lat: 40.7484213,
-      lng: -73.9878416,
-    },
-    creator: 'u2',
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
 
