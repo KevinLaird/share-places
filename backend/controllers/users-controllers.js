b039f682f62144bc105b9c5a87c5ce6244d413cc
KevinLaird <kevinlaird16@gmail.com>
2022-10-20 13:02:33 -0700

    Added return next(error) to login check

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -71,18 +71,23 @@ const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   let existingUser;
+
   try {
     existingUser = await User.findOne({ email: email });
   } catch (err) {
-    const error = new HttpError('Login failed. Please try again later.', 500);
+    const error = new HttpError(
+      'Login in failed, please try again later.',
+      500,
+    );
     return next(error);
   }
 
   if (!existingUser || existingUser.password !== password) {
     const error = new HttpError(
-      'Invalid credentials. Could not log you in',
+      'Invalid credentials, could not log you in.',
       401,
     );
+    return next(error);
   }
 
   res.json({ message: 'Logged in!' });
