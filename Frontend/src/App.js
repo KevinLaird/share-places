9e0eb3d0f99c517cfb3c6c7a2ad2720d0ee6e21b
KevinLaird <kevinlaird16@gmail.com>
2022-11-03 15:31:12 -0700

    App now loads in chunks

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-import Users from './user/pages/Users';
-import Authenticate from './user/pages/Authenticate';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
 import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks/auth-hook';
+
+const Users = React.lazy(() => import('./user/pages/Users'));
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'));
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'));
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'));
+const Authenticate = React.lazy(() => import('./user/pages/Authenticate'));
 
 function App() {
   const { token, login, logout, userId } = useAuth();
@@ -49,7 +51,15 @@ function App() {
       >
         <MainNavigation />
         <main>
-          <Routes>{routes}</Routes>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            <Routes>{routes}</Routes>
+          </Suspense>
         </main>
       </AuthContext.Provider>
     </>
