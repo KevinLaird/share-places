8dbeba317373cad6c5467fa47bfb5ab3fb11176d
KevinLaird <kevinlaird16@gmail.com>
2022-11-03 12:47:27 -0700

    Added authorization to updating place

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -138,6 +138,14 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError(
+      'You are not authorized to edit this place',
+      401,
+    );
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
